9a3bc64466e03a55ec4731ae1991addb4a7a71cc
Rob Halff <rob.halff@gmail.com>
2013-12-29 02:14:47 +0100

    listen to the events

--- nodes/webrtc/node.js
+++ nodes/webrtc/node.js
@@ -1,1 +1,55 @@
-output = { webrtc: new webrtc(input) };
+output = function(cb) {
+
+  var wrtc = new webrtc(input);
+
+  // our direct port.
+  cb({
+    webrtc: wrtc
+  });
+
+  // send local stream
+  wrtc.on('localStream', function(stream) {
+     cb({ localStream: stream });
+  });
+
+  wrtc.on('localStreamStopped', function() {
+     cb({ localStreamStopped: true });
+  });
+
+  wrtc.on('audioOn', function() {
+     cb({ audio: true });
+  });
+
+  wrtc.on('audioOff', function() {
+     cb({ audio: false });
+  });
+
+  wrtc.on('videoOn', function() {
+     cb({ video: true });
+  });
+
+  wrtc.on('videoOff', function() {
+     cb({ video: false });
+  });
+
+  wrtc.on('speaking', function() {
+     cb({ speaking: true });
+  });
+
+  wrtc.on('stoppedSpeaking', function() {
+     cb({ speaking: false });
+  });
+
+  wrtc.on('message', function(message) {
+     cb({ message: message });
+  });
+
+  wrtc.on('peerStreamAdded', function(peer) {
+     cb({ peerStreamAdded: peer });
+  });
+
+  wrtc.on('peerStreamRemoved', function(peer) {
+     cb({ peerStreamRemoved: peer });
+  });
+
+};
