f26c36141d45f66cd75b6a9f1aaaa17ff9745118
Rob Halff <rob.halff@gmail.com>
2013-12-30 02:25:24 +0100

    add some

---
+++ nodes/getUserMedia/node.js
@@ -0,0 +1,17 @@
+var speachEvents = hark(input.stream);
+
+output = function(cb) {
+
+  speechEvents.on('speaking', function() {
+    cb({ speaking: true });
+  });
+
+  speechEvents.on('volume_change', function(volume, threshold) {
+    cb({ volume: volume, treshold: treshold});
+  });
+
+  speechEvents.on('stopped_speaking', function() {
+    cb({ speaking: false });
+  });
+
+};
